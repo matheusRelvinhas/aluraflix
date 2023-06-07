50595901507b07c0566beda29fea5dbaf79acefa
MatheusRelvinhas <matheusrelvinhas@gmail.com>
2023-06-07 10:33:06 -0300

    slider

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Carousel.css';
 import Slider from './Slider/Slider';
-import VideoCard from './VideoCard/VideoCard';
 
 const Carousel = (/*{category , images*/) => {  
   
@@ -14,28 +13,39 @@ const Carousel = (/*{category , images*/) => {
   }
 
   const dataCard = [ // informações do card(main)
-    { // images 512x512
-      title: 'Peça pelo Whats',
-      text: 'Mande uma mensagem e vamos te atender',
-      image: 'https://i.postimg.cc/kXkSMJTx/whats01-card.webp'
-    },
-    {
-      title: 'Peça pelo Ifood',
-      text: 'Consulte nossas promoções no ifood',
-      image: 'https://i.postimg.cc/qMgcMZ4d/ifood01-card.webp'
-    }
+    [
+      { // images 512x512
+        title: 'Peça pelo Whats',
+        text: 'Mande uma mensagem e vamos te atender',
+        image: 'https://i.postimg.cc/kXkSMJTx/whats01-card.webp'
+      },
+      {
+        title: 'Peça pelo Ifood',
+        text: 'Consulte nossas promoções no ifood',
+        image: 'https://i.postimg.cc/kXkSMJTx/whats01-card.webp'
+      }
+    ],
+    [
+      { // images 512x512
+        title: 'Peça pelo Whats2',
+        text: 'Mande uma mensagem e vamos te atender2',
+        image: 'https://i.postimg.cc/qMgcMZ4d/ifood01-card.webp'
+      },
+      {
+        title: 'Peça pelo Ifood2',
+        text: 'Consulte nossas promoções no ifood2',
+        image: 'https://i.postimg.cc/qMgcMZ4d/ifood01-card.webp'
+      }
+    ],
   ]
   
   return (
     <div className='card'>
-      <Slider images={dataCarousel.images} category={dataCarousel.category}/>
-      <div className='card-content'>
-        {dataCard.map(card => <VideoCard
-          title= {card.title}
-          text= {card.text}
-          image= {card.image}
-        />)}
-      </div>
+      <Slider 
+        images={dataCarousel.images} 
+        category={dataCarousel.category}
+        dataCard={dataCard}
+      />
     </div>
   ); 
 }
--- src/Components/Carousel/Slider/Slider.js
+++ src/Components/Carousel/Slider/Slider.js
@@ -1,32 +1,43 @@
 import React, { useState } from 'react';
+import VideoCard from '../VideoCard/VideoCard';
 import './Slider.css';
 
-const Slider = ({ images, category }) => {
+const Slider = ({ images, category, dataCard }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [currentIndex1, setCurrentIndex1] = useState(0);
 
   const goToNextSlide = () => {
     setCurrentIndex((currentIndex + 1) % images.length);
-    setCurrentIndex1((currentIndex1 + 1) % category.length);
+
   };
 
   const goToPreviousSlide = () => {
     setCurrentIndex((currentIndex - 1 + images.length) % images.length);
-    setCurrentIndex1((currentIndex1 - 1 + category.length) % category.length);
+
   };
 
   return (
     <div className="carousel">
-      <button className="carousel-button prev" onClick={goToPreviousSlide}>
-        &#8249;
-      </button>
-      <img className="carousel-image" src={images[currentIndex]} alt="Slide" />
-      <button className="carousel-button next" onClick={goToNextSlide}>
-        &#8250;
-      </button>
-      <h2>{category[currentIndex1]}</h2>
+      <div>
+        <button className="carousel-button prev" onClick={goToPreviousSlide}>
+          &#8249;
+        </button>
+        <img className="carousel-image" src={images[currentIndex]} alt="Slide" />
+        <button className="carousel-button next" onClick={goToNextSlide}>
+          &#8250;
+        </button>
+        <h2>{category[currentIndex]}</h2>
+      </div>
+      <div className='card-content'>
+        {dataCard[currentIndex].map(card => <VideoCard
+          title= {card.title}
+          text= {card.text}
+          image= {card.image}
+        />)}
+      </div>
     </div>
   );
 };
 
 export default Slider;
+
+
